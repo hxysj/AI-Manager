e3ddb454a317ca816f05f652b2087c7a3c1a4fe8
hxysj <liuzunbo@readboy.com>
2026-06-05 14:48:03 +0800

    feat: 删除原本框架的所有内容

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue"
-import { ElDatePicker, ElInput } from "element-plus"
-import "element-plus/es/components/date-picker/style/css"
-import "element-plus/es/components/input/style/css"
-import App from "./App.vue"
-import "./styles/app.less"
-
-createApp(App).use(ElDatePicker).use(ElInput).mount("#app")
